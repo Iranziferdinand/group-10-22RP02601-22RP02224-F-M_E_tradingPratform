52cd730ffb27eb361afbc49713ac6f4c7e970d4f
Manirakizadaniel <164475288+Manirakizadaniel@users.noreply.github.com>
2025-04-14 20:54:27 +0200

    dashboard.blade commited have been completed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Admin Dashboard') }}</div>
-
+                
                 <div class="card-body">
                     <!-- Statistics Cards -->
                     <div class="row mb-4">
